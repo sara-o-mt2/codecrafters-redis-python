4a6c177ca070cea39323e7bd8e60c86c841d0900
sara-o-mt2 <32122648+sara-o-mt2@users.noreply.github.com>
2024-12-17 22:21:40 +0900

    feat: implement client handling in a separate thread

--- app/main.py
+++ app/main.py
@@ -1,21 +1,26 @@
 import socket
+import threading
 
 from app.util import convert_into_resp
 
-def main():
-    print("Logs from your program will appear here!")
+def handle_client(client: socket.socket, addr: tuple[str, int]) -> None:
+    with client:
+        while True:
+            request = client.recv(512)
+            data_lines = request.decode().split('\n')
 
+            for data in data_lines:
+                if "ping" in data.lower():
+                    client.send(convert_into_resp("+PONG\r\n"))
+
+def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client, addr = server_socket.accept()
 
-    while True:
-        request = client.recv(512)
-        data_lines = request.decode().split('\n')
-
-        for data in data_lines:
-            print(data)
-            if "ping" in data.lower():
-                client.send(convert_into_resp("+PONG\r\n"))
+    client_thread = threading.Thread(
+        target=handle_client, args=[client, addr]
+    )
+    client_thread.start()
 
 
 if __name__ == "__main__":
