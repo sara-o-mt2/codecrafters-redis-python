3be51e225721ce9d293339e5421b9b8a61b91c2f
sara-o-mt2 <32122648+sara-o-mt2@users.noreply.github.com>
2024-12-30 01:13:40 +0900

    feat: Expiry

---
+++ app/domain/entities/command_set_args.py
@@ -0,0 +1,15 @@
+class CommandSetArgs:
+    key: str
+    value: str
+    expiry: bool
+    interval: int # milliseconds
+
+    def __init__(self, args: list[str]):
+        self.key = args[0]
+        self.value = args[1]
+        if args[2] == "px" and args[3].isnumeric():
+            self.expiry = True
+            self.interval = int(args[3])
+        else:
+            self.expiry = False
+            self.interval = 0
--- app/main.py
+++ app/main.py
@@ -1,38 +1,50 @@
-import socket
+import socket, time
 import threading
 
 from app.domain.entities.redis_client_requests import RedisClientRequests
 from app.domain.entities.redis_responses import RedisResponses
 from app.domain.entities.resp_command import RESPCommand
+from app.domain.entities.command_set_args import CommandSetArgs
 
 
-db = {}
+table_store = {}
+table_expiry = {} 
 
 def handle_client(client: socket.socket, addr: tuple[str, int]) -> None:
     with client:
-        while True:
-            row_requests = client.recv(512)
-            if not row_requests:
-                break
+        row_requests = client.recv(512)
+        if not row_requests:
+            return
 
-            requests = RedisClientRequests(row_requests)
-            command = RESPCommand(requests.decode())
+        requests = RedisClientRequests(row_requests)
+        command = RESPCommand(requests.decode())
 
-            if command.command == "PING":
-                response = RedisResponses("PONG")
-                client.send(response.encode())
-            elif command.command == "ECHO":
-                response = RedisResponses(command.arguments[0])
-                client.send(response.encode())
-            elif command.command == "SET":
-                db[command.arguments[0]] = command.arguments[1]
-                response = RedisResponses("OK")
-                client.send(response.encode())
-            elif command.command == "GET":
-                response = RedisResponses(db.get(command.arguments[0]))
-                client.send(response.encode())
-            else:
-                break
+        if command.command == "PING":
+            response = RedisResponses("PONG")
+            client.send(response.encode())
+        elif command.command == "ECHO":
+            response = RedisResponses(command.arguments[0])
+            client.send(response.encode())
+        elif command.command == "SET":
+            command_set_args = CommandSetArgs(command.arguments)
+            table_store[command_set_args.key] = command_set_args.value
+            if command_set_args.expiry:
+                table_expiry[command_set_args.key] = time.time() * 1000 + command_set_args.interval
+            response = RedisResponses("OK")
+            client.send(response.encode())
+        elif command.command == "GET":
+            if command.arguments[0] in table_expiry:
+                if table_expiry[command.arguments[0]] < time.time() * 1000:
+                    table_store.pop(command.arguments[0], None)
+                    table_expiry.pop(command.arguments[0], None)
+                    response = RedisResponses("$-1")
+                    client.send(response.encode())
+                    return
+
+            response = RedisResponses(table_store.get(command.arguments[0]))
+            client.send(response.encode())
+        else:
+            return
 
 def main() -> None:
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
