90b70b90f7cf1192a4da60062f9b4e60615f9eba
sara-o-mt2 <32122648+sara-o-mt2@users.noreply.github.com>
2024-12-29 23:08:42 +0900

    feat: Implement the ECHO command

--- app/main.py
+++ app/main.py
@@ -7,11 +7,16 @@ def handle_client(client: socket.socket, addr: tuple[str, int]) -> None:
     with client:
         while True:
             request = client.recv(512)
-            data_lines = request.decode().split('\n')
+            if not request:
+                break
+
+            data_lines = request.decode().split('\r\n')
 
             for data in data_lines:
-                if "ping" in data.lower():
+                if "ping" == data[1].lower():
                     client.send(convert_into_resp("+PONG\r\n"))
+                elif "echo" in data[1].lower():
+                    client.send(convert_into_resp("+" + data[3::2] + "\r\n"))
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
